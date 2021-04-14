5d12c04cbeab11e2e3921f18427453f707d72779
Elias Mawa <elias@emawa.io>
2021-04-13 23:19:01 -0600

    [skip ci] tweak tests

---
+++ spec/ext/footer.spec.ts
@@ -0,0 +1,9 @@
+import { render } from '../lib/render';
+
+const filename = 'footer.scss';
+
+describe(`src/ext/${filename}`, () => {
+	it('compiles to CSS', () => {
+		return render(`src/ext/_${filename}`);
+	})
+});
